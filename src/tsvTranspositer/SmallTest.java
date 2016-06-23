8ca66723049d0c9dcafcbad432d06064111ba572
Emmanuel HAMM <Emmanuel.hamm@enerdata.net>
2016-06-23 16:37:26 +0200

    Rajout paramètres en début de main, fonctionne pour exempleProPerso1.tsv

--- src/tsvTranspositer/SmallTest.java
+++ src/tsvTranspositer/SmallTest.java
@@ -7,26 +7,41 @@ public class SmallTest {
 
 	public static void main(String[] args) {
 
-		InputFile ex1 = new InputFile("exemplePerso1.tsv", 2);
+		String inputFile = "exempleProPerso1.tsv"; // TODO : nom du fichier d'entrée
+		
+		String outputFile = "resultatProPerso1.tsv"; // TODO : nom du fichier de sortie
+		
+		int linesToCopy = 0; // TODO : nombre de lignes ("header") en début de fichier à recopier
+		
+		String valueName = "quantity" ; // TODO : nom de la colonne donnant les quantités
+		
+		String periodName = "period" ; // TODO : nom de la colonne donnant les périodes
+		
+		int serieNb = 1 ; // TODO : nombre de colonnes correspondant (nombre de colonnes avant les données : elles seront recopiées)
+		
+		
+		InputFile ex1 = new InputFile(inputFile, linesToCopy); 
 		ex1.readHead();
 		StringArray2D head = new StringArray2D(ex1.getHeadFile()); // TODO : TBR
 		head.print(); // TODO : TBR
 		
-		OutputFile ex2 = new OutputFile("resultat1.tsv");
+		OutputFile ex2 = new OutputFile(outputFile);
 		
 		ex2.writeHead(ex1.getHeadFile());
 		
-		HeadOfValuesHandler handler = new HeadOfValuesHandler(ex1.readLine(), "Sold", "Year", 3);
+		HeadOfValuesHandler handler = new HeadOfValuesHandler(ex1.readLine(), valueName, periodName, serieNb);
 		ex2.writeLine(handler.createOutputHOV());
 		
 		StringArray years = new StringArray(handler.getYears()); // TODO : TBR
 		years.print(); // TODO : TBR
 		
-		
+		int i = 1; // TODO : TBR
 		
 		while (!ex1.isAllDone()) {
 			
-			CommonLine cl1 = new CommonLine(ex1.readLine(), handler.getYears(), 3);
+			System.out.println("Gestion de la ligne " + i);
+			
+			CommonLine cl1 = new CommonLine(ex1.readLine(), handler.getYears(), serieNb);
 			
 			StringArray2D l12D = new StringArray2D(cl1.exportOutputLines()); // TODO : TBR
 			l12D.print(); // TODO : TBR
